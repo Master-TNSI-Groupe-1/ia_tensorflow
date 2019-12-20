e6ff6726a40cc1374c247a12ac3d2d7efdee8568
Julien B <julien.basquin@hotmail.com>
2019-12-20 15:13:36 +0100

    Prediction

--- script.js
+++ script.js
@@ -14,25 +14,158 @@ const request = require('request')
 
 async function runModel(mapData) {
     console.log(mapData)
+
+    const values = mapData.map(d => ({
+        x: d.hours,
+        y: d.users,
+      }));
+
+    tfvis.render.scatterplot(
+        {name: 'Test'},
+        {values}, 
+        {
+          xLabel: 'Hours',
+          yLabel: 'Users',
+          height: 300
+        }
+    );
+
+    const tensor = convertToTensor(mapData)
+    console.log(tensor)
+    const {hours, users} = tensor
+    console.log(hours, users)
+
+    const model = createModel()
+    console.log("Model created")
+    console.log(model)
+    tfvis.show.modelSummary({name: 'Model Summary'}, model);
+
+    await trainModel(model, hours, users)
+    console.log("Done training")
+
+    testModel(model, mapData, tensor);
 }
 
+
+function convertToTensor(data) {
+    return tf.tidy(() => {
+        tf.util.shuffle(data)
+        
+        const hours = data.map(d => d.hours)
+        const users = data.map(d => d.users);
+        
+        const hoursTensor = tf.tensor2d(hours, [hours.length, 1])
+        const usersTensor = tf.tensor2d(users, [users.length, 1])
+        
+        const hoursMax = hoursTensor.max()
+        const hoursMin = hoursTensor.min()
+        const usersMax = usersTensor.max()
+        const usersMin = usersTensor.min()
+    
+        const normalizedHours = hoursTensor.sub(hoursMin).div(hoursMax.sub(hoursMin))
+        const normalizedusers = usersTensor.sub(usersMin).div(usersMax.sub(usersMin))
+    
+        return {
+            hours : normalizedHours,
+            users : normalizedusers,
+            hoursMax,
+            hoursMin,
+            usersMax,
+            usersMin
+        }
+    });
+}
+
+function createModel() {
+    const model = tf.sequential(); 
+    
+    model.add(tf.layers.dense({inputShape: [1], units: 40, useBias: true, activation: 'relu'}));
+    // model.add(tf.layers.dense({units: 50, useBias: true, activation: 'relu'}));
+    model.add(tf.layers.dense({units: 20, useBias: true, activation: 'relu'}));
+    model.add(tf.layers.dense({units: 1, useBias: true, activation: 'relu'}));
+  
+    return model;
+}
+
+async function trainModel(model, hours, users) {
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse'],
+      });
+      
+      const batchSize = 72;
+      const epochs = 200;
+      
+      return await model.fit(hours, users, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+          { name: 'Training Performance' },
+          ['loss', 'mse'], 
+          { height: 200, callbacks: ['onEpochEnd'] }
+        )
+      });
+}
+
+async function testModel(model, inputData, normalizationData) {
+    const {hoursMax, hoursMin, usersMin, usersMax} = normalizationData;  
+    
+    const [xs, preds] = tf.tidy(() => {
+      
+      const xs = tf.linspace(0, 1, 23);      
+      const preds = model.predict(xs.reshape([23, 1]));      
+      
+      const unNormXs = xs
+        .mul(hoursMax.sub(hoursMin))
+        .add(hoursMin);
+      
+      const unNormPreds = preds
+        .mul(usersMax.sub(usersMin))
+        .add(usersMin);
+      
+      // Un-normalize the data
+      return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+    
+   
+    const predictedPoints = Array.from(xs).map((val, i) => {
+      return {x: val, y: preds[i]}
+    });
+    
+    const originalPoints = inputData.map(d => ({
+      x: d.hours, y: d.users,
+    }));
+    
+    
+    tfvis.render.scatterplot(
+      {name: 'Model Predictions vs Original Data'}, 
+      {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
+      {
+        xLabel: 'hours',
+        yLabel: 'users',
+        height: 300
+      }
+    );
+  }
+
 async function run() {
-    new Promise( (resolve, reject) => {
-        let _timestamp = Date.now() - (86400000 * 30)
-        let _mapData = new Map()
+    let _timestamp = Date.now() - (86400000 * 30)
+    let _data = new Array()
+    
+    request(`http://3.87.54.32:8081/api/daysAfter/3/${_timestamp}`, { json: true}, (err, res, body) => {
+        body.forEach( (object) => {
+            let _map = {}
+            let _datetime = new Date(object.currentdate)
+            
+            _map["hours"] = _datetime.getHours()
+            _map["users"] = object.numberuser
 
-        for (let i = 0; i <= 23; i++) _mapData.set(i, new Array())
-
-        request(`http://3.87.54.32:8081/api/daysAfter/3/${_timestamp}`, { json: true}, (err, res, body) => {
-            body.forEach( (object) => {
-                let _datetime = new Date(object.currentdate)
-                _mapData.get(_datetime.getHours()).push(object.numberuser);
-            })
-
-            resolve(_mapData)
+            _data.push(_map)
         })
-    }).then((data) => {
-        runModel(data)
+
+        runModel(_data)
     })
 }
 
