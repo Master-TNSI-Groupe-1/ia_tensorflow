5851590cb816be426fceb7c7cf5af19d002845e7
Skryle <deschamps.ghislain@gmail.com>
2019-12-20 11:22:20 +0100

    Initialisation

--- script.js
+++ script.js
@@ -12,24 +12,28 @@ import 'babel-polyfill'
 
 const request = require('request')
 
-async function getData(_timestamp) {
-    var mapData = new Map()
-
-    for (let i = 0; i <= 23; i++) mapData.set(i, new Array())
-
-    await request(`http://3.87.54.32:8081/api/daysAfter/3/${_timestamp}`, { json: true}, (err, res, body) => {
-        body.forEach( (object) => {
-            let _datetime = new Date(object.currentdate)
-            mapData.get(_datetime.getHours()).push(object.numberuser);
-        })
-
-    })
+async function runModel(mapData) {
+    console.log(mapData)
 }
 
-
 async function run() {
-    const timestamp = Date.now() - (86400000 * 30)
-    await getData(timestamp)
+    new Promise( (resolve, reject) => {
+        let _timestamp = Date.now() - (86400000 * 30)
+        let _mapData = new Map()
+
+        for (let i = 0; i <= 23; i++) _mapData.set(i, new Array())
+
+        request(`http://3.87.54.32:8081/api/daysAfter/3/${_timestamp}`, { json: true}, (err, res, body) => {
+            body.forEach( (object) => {
+                let _datetime = new Date(object.currentdate)
+                _mapData.get(_datetime.getHours()).push(object.numberuser);
+            })
+
+            resolve(_mapData)
+        })
+    }).then((data) => {
+        runModel(data)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', run)
